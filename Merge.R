d1a62b07d95a0cbead3c544aef96bd5ba7e29e72
Gwen-Laudel <gwen.laudel@berkeley.edu>
2022-06-24 16:48:43 -0600

    Installing packages for Gwen and Merge.

---
+++ Merge.R
@@ -0,0 +1,14 @@
+packages <- c("tidyverse",    # ggplot, dplyr, etc. 
+              "TSA",          # time series stuff
+              "lubridate",    # date time 
+              "roxygen2"      # function documentation
+)    
+
+# check if packages are installed, if not, install
+install.packages(setdiff(packages, rownames(installed.packages())))  
+
+# load packages
+library(tidyverse)
+library(TSA)
+library(lubridate)
+library(roxygen2)
